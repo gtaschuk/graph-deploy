78bc8f82fa708d99d86f1ae55ce105bd22bf51b1
Greg Taschuk <gregorytaschuk@gmail.com>
2021-02-22 17:57:34 -0800

    WIP

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const core = require("@actions/core");
 const { execSync } = require("child_process");
 
 (async () => {
-  let authCmd = `./node_modules/.bin/graph auth https://api.thegraph.com/deploy/${core.getInput('graph_access_token')}`
-  let deployCmd = `./node_modules/.bin/graph deploy ${core.getInput('graph_account')}/${core.getInput('graph_subgraph_name')} ${core.getInput('graph_config_file')} --ipfs https://api.thegraph.com/ipfs/ --node https://api.thegraph.com/deploy/`
+  let authCmd = `node_modules/.bin/graph auth https://api.thegraph.com/deploy/${core.getInput('graph_access_token')}`
+  let deployCmd = `node_modules/.bin/graph deploy ${core.getInput('graph_account')}/${core.getInput('graph_subgraph_name')} ${core.getInput('graph_config_file')} --ipfs https://api.thegraph.com/ipfs/ --node https://api.thegraph.com/deploy/`
 
   try {
     console.log("Authenticating with access token...")
