f73d97936359d6a21b4ed436d7866d6bed20e0c2
Greg Taschuk <gregorytaschuk@gmail.com>
2021-02-22 18:16:56 -0800

    find npm bin folder

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const core = require("@actions/core");
 const { execSync } = require("child_process");
 
 (async () => {
-  let authCmd = `node_modules/.bin/graph auth https://api.thegraph.com/deploy/${core.getInput('graph_access_token')}`
-  let deployCmd = `node_modules/.bin/graph deploy ${core.getInput('graph_account')}/${core.getInput('graph_subgraph_name')} ${core.getInput('graph_config_file')} --ipfs https://api.thegraph.com/ipfs/ --node https://api.thegraph.com/deploy/`
+  let npmBin = execSync(`npm bin`).toString().trim()
+  let authCmd = `${npmBin}/graph auth https://api.thegraph.com/deploy/${core.getInput('graph_access_token')}`
+  let deployCmd = `${npmBin}/graph deploy ${core.getInput('graph_account')}/${core.getInput('graph_subgraph_name')} ${core.getInput('graph_config_file')} --ipfs https://api.thegraph.com/ipfs/ --node https://api.thegraph.com/deploy/`
 
   try {
+    console.log("using npm bin folder", npmBin)
     console.log("Authenticating with access token...")
     execSync(authCmd)
     console.log("Deploying to graph protocol...")
